18e01687bffda5b761a875cd143d82f84b872016
jerxy <jerxylol@gmail.com>
2023-05-26 13:03:36 +0200

    Send email using the form

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,12 +1,21 @@
 import streamlit as st
+import send_email
 
 st.title("Contact me")
 
 with st.form(key="email_form"):
     user_email = st.text_input("Your email adress")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email} 
+{raw_message}
+"""
     button = st.form_submit_button("Submit")
     if button:
-        massage = message + user_email
-        print(button)
-        print(" Buttoned ")
+        sending = send_email.send_email(message)
+        if sending == "error":
+            st.write("you have to enter your message without ř/á/í/ů.. symbols")
+        else:
+            st.info("your email has been sent")
--- send_email.py
+++ send_email.py
@@ -1,22 +1,18 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "s1fam0n3@gmail.com"
-password = "azbychwijzpsbcyh"
+def send_email(message):
+    try:
+        host = "smtp.gmail.com"
+        port = 465
+        username = "s1fam0n3@gmail.com"
+        password = "azbychwijzpsbcyh"
+        reciever = "s1fam0n3@gmail.com"
+        context = ssl.create_default_context()
 
-reciever = "s1fam0n3@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Email from Portfolio app
-How are you?
-Hey, hi, hello.
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, reciever, message)
+        with smtplib.SMTP_SSL(host=host, port=port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, reciever, message)
+    except UnicodeError:
+        return "error"
