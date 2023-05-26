5381cd8dd79d0c8b94ad42f024b4a057c362bb39
jerxy <jerxylol@gmail.com>
2023-05-26 12:18:48 +0200

    Send email

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Contact me:")
+st.title("Contact me")
+
+with st.form(key="email_form"):
+    user_email = st.text_input("Your email adress")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+    if button:
+        massage = message + user_email
+        print(button)
+        print(" Buttoned ")
